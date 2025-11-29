0d35b6ea04340413055425b2eaba46b9c9d96209
espadara <espadara@pirate.capn.gg>
2025-11-29 16:59:45 +0100

    [ADD] main ping loop

---
+++ src/ping_loop.c
@@ -0,0 +1,126 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                        ______                                              */
+/*                     .-"      "-.                                           */
+/*                    /            \                                          */
+/*        _          |              |          _                              */
+/*       ( \         |,  .-.  .-.  ,|         / )                             */
+/*        > "=._     | )(__/  \__)( |     _.=" <                              */
+/*       (_/"=._"=._ |/     /\     \| _.="_.="\_)                             */
+/*              "=._ (_     ^^     _)"_.="                                    */
+/*                  "=\__|IIIIII|__/="                                        */
+/*                 _.="| \IIIIII/ |"=._                                       */
+/*       _     _.="_.="\          /"=._"=._     _                             */
+/*      ( \_.="_.="     `--------`     "=._"=._/ )                            */
+/*       > _.="                            "=._ <                             */
+/*      (_/                                    \_)                            */
+/*                                                                            */
+/*      Filename: ping_loop.c                                                 */
+/*      By: espadara <espadara@pirate.capn.gg>                                */
+/*      Created: 2025/11/29 16:41:04 by espadara                              */
+/*      Updated: 2025/11/29 16:43:33 by espadara                              */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_ping.h"
+
+/* Helper: Calculate time difference in milliseconds */
+static double get_time_diff(struct timeval *start, struct timeval *end)
+{
+  double s;
+  double e;
+
+  s = start->tv_sec * 1000.0 + start->tv_usec / 1000.0;
+  e = end->tv_sec * 1000.0 + end->tv_usec / 1000.0;
+  return (e - s);
+}
+
+static void update_stats(t_ping *ping, double rtt)
+{
+  ping->stats.rx_packets++;
+  ping->stats.t_sum += rtt;
+  ping->stats.t_sq_sum += (rtt * rtt); // For standard deviation later
+
+  if (ping->stats.rx_packets == 1 || rtt < ping->stats.t_min)
+    ping->stats.t_min = rtt;
+  if (rtt > ping->stats.t_max)
+    ping->stats.t_max = rtt;
+}
+
+static void print_reply(t_ping *ping, char *buf, ssize_t ret, double rtt)
+{
+  struct ip *ip;
+  struct icmp *icmp;
+  char src_ip[INET_ADDRSTRLEN];
+
+  // IP Header is at the start of the buffer
+  ip = (struct ip *)buf;
+  // ICMP Header starts after the IP Header (length is in 32-bit words)
+  icmp = (struct icmp *)(buf + (ip->ip_hl << 2));
+
+  // Convert Source IP to string
+  inet_ntop(AF_INET, &ip->ip_src, src_ip, INET_ADDRSTRLEN);
+
+  // Format: "64 bytes from 1.2.3.4: icmp_seq=1 ttl=64 time=0.045 ms"
+  sea_printf("%ld bytes from %s: icmp_seq=%d ttl=%d time=%.3f ms\n",
+             ret - (ip->ip_hl << 2), // Payload size (Total - IP Header)
+             src_ip,
+             ntohs(icmp->icmp_seq),
+             ip->ip_ttl,
+             rtt
+    );
+}
+
+void loop_ping(t_ping *ping)
+{
+  char            send_buf[PING_PKT_SIZE];
+  char            recv_buf[RECV_BUFFER_SIZE];
+  struct sockaddr_in from_addr;
+  socklen_t       addr_len;
+  int             seq;
+  ssize_t         ret;
+  struct timeval  curr_time;
+  struct ip       *ip_header;
+  struct icmp     *icmp_header;
+
+  seq = 0;
+  while (1)
+    {
+      // --- SEND ---
+      sea_bzero(send_buf, PING_PKT_SIZE);
+      craft_packet(ping, send_buf, ++seq);
+      if (sendto(ping->sockfd, send_buf, PING_PKT_SIZE, 0,
+                 (struct sockaddr *)&ping->dest_addr, sizeof(ping->dest_addr)) < 0)
+        {
+          sea_printf("ft_ping: sendto error\n");
+            // Don't exit, just try next loop
+        }
+      else
+          ping->stats.tx_packets++;
+      // We wait for a reply. Using recvfrom which blocks until timeout (set in init).
+        addr_len = sizeof(from_addr);
+        ret = recvfrom(ping->sockfd, recv_buf, RECV_BUFFER_SIZE, 0,
+                       (struct sockaddr *)&from_addr, &addr_len);
+        if (ret > 0)
+          {
+            // Unpack IP Header to find ICMP
+            ip_header = (struct ip *)recv_buf;
+            icmp_header = (struct icmp *)(recv_buf + (ip_header->ip_hl << 2));
+
+            // Check: Is it an Echo Reply (Type 0) and is it OURS (ID match)?
+            if (icmp_header->icmp_type == ICMP_ECHOREPLY &&
+                icmp_header->icmp_id == htons(ping->pid))
+              {
+                // Retrieve the timestamp we hid in the payload
+                struct timeval *sent_time = (struct timeval *)(recv_buf +
+                          (ip_header->ip_hl << 2) + sizeof(struct icmp));
+                gettimeofday(&curr_time, NULL);
+                double rtt = get_time_diff(sent_time, &curr_time);
+                update_stats(ping, rtt);
+                print_reply(ping, recv_buf, ret, rtt);
+              }
+          }
+        // Standard ping waits 1 second between packets.
+        sleep(1);
+    }
+}
